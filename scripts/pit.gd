3aaea037e4c4287c5236ae28f2fce8d2f6974bb0
Aero48 <56200404+Aero48@users.noreply.github.com>
2024-04-02 13:29:34 -0400

    finished level 4

--- scripts/pit.gd
+++ scripts/pit.gd
@@ -4,7 +4,6 @@ extends Area2D
 
 func _on_body_entered(body):
 	if body.is_in_group("player"):
-		
 		get_node("/root/Platformer/GameController").player_death()
-	else:
+	elif body.is_in_group("enemies"):
 		body.queue_free()
