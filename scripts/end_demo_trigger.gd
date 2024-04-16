545e58d8756a83dec872af6360fe370f1e961f6c
Aero48 <56200404+Aero48@users.noreply.github.com>
2024-04-16 16:28:16 -0400

    patched level scenes together

--- scripts/end_demo_trigger.gd
+++ scripts/end_demo_trigger.gd
@@ -2,4 +2,4 @@ extends Area2D
 
 func _on_body_entered(body):
 	if body.is_in_group("player"):
-		get_tree().change_scene_to_file("res://levels/temp.tscn")
+		get_tree().change_scene_to_file("res://levels/tunnel.tscn")
