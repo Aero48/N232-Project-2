36effa2fec856eb4ada40fb38d76a8858b4a8e48
Aero48 <logandouglass9@gmail.com>
2024-04-23 19:36:15 -0400

    fixed spikes kinda and added wasd controls

--- scripts/spikes.gd
+++ scripts/spikes.gd
@@ -3,4 +3,4 @@ extends StaticBody2D
 func _on_area_2d_body_entered(body):
 	if body.is_in_group("player"):
 		if body.currentEffect != 3:
-			get_node("/root/GameController").playerDeath()
+			get_node("/root/GameController").player_death()
