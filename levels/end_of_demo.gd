55845f4f591b72dafd58b931a47fa3c489429f9e
Aero48 <56200404+Aero48@users.noreply.github.com>
2024-04-26 15:13:45 -0400

    small bug fix in final screen

--- levels/end_of_demo.gd
+++ levels/end_of_demo.gd
@@ -1,7 +1,10 @@
 extends Node2D
 
+@export var backBtn: Button
+
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	backBtn.grab_focus()
 
 func _on_quit_btn_pressed():
 	get_tree().change_scene_to_file("res://levels/title.tscn")
